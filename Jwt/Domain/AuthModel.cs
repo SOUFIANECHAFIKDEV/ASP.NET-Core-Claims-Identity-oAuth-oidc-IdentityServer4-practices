4d1251b5f9c7213a72b96ca7f8dab55220547633
MEN\SQLI.DotNetUser8 <schafik@sqli.com>
2021-11-13 21:30:05 +0100

    Registration EndPoint

--- Jwt/Domain/AuthModel.cs
+++ Jwt/Domain/AuthModel.cs
@@ -6,7 +6,7 @@ namespace Jwt.Domain
     public class AuthModel
     {
         public string Message { get; set; }
-        public int IsAuthenticated { get; set; }
+        public bool IsAuthenticated { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
         public List<string> Roles { get; set; }
